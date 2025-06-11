c795f046bdc2812bb165502673105411571de0b2
ErnestoTSantos <ernesto.terra2003@gmail.com>
2025-06-11 19:25:05 -0300

    fix: remove main comments

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from lexical_interpreter import ElgolLexer
 from syntactic_interpreter import ElgolParser
 
 
-"""def run_lexical_analysis(code: str):
+def run_lexical_analysis(code: str):
     print("Análise Léxica:")
     lexer = ElgolLexer()
     tokens = lexer.tokenize(code)
 
     for token in tokens:
         print(token)
-"""
+
 
 def run_syntactic_analysis(code: str):
     print("\nCode Compile:")
@@ -22,7 +22,7 @@ def main():
     with open("elgol_file.txt", "r") as file:
         code = file.read()
 
-    #run_lexical_analysis(code)
+    run_lexical_analysis(code)
     run_syntactic_analysis(code)
 
 
